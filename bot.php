71d46ed1bca46e0174357f0a7b7fa79306a2af98
Bernardo <bernardoroepke2704@gmail.com>
2023-07-17 20:21:11 -0300

    17/07

--- bot.php
+++ bot.php
@@ -3,7 +3,11 @@
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
 use Discord\WebSockets\Event;
-use Discord\Parts\WebSockets\VoiceStateUpdate;
+use Discord\Parts\Embed\Embed;
+use Discord\Builders\Components\ActionRow;
+use Discord\Builders\Components\Button;
+use Discord\Builders\MessageBuilder;
+use Discord\Parts\Interactions\Interaction;
 
 include __DIR__.'/vendor/autoload.php';
 
@@ -11,35 +15,52 @@ $envPath = realpath(dirname(__FILE__) . '/env.ini');
 $env = parse_ini_file($envPath);
 
 $discord = new Discord([
-    'token' => $env['token'],
-    'loadAllMembers' => false,
+    'token' => $env['token2'],
 ]);
 
 $discord->on('ready', function (Discord $discord) {
 
     echo "O bot está ouvindo";
 
-    $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) { #TRANSFORMAR TUDO ISSO EM CASES
+    $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
         if($message->author->bot) return;
 
         $content = $message->content;
-        if(str_contains($content, '!regras')) {
-            $message->channel->sendMessage("{$message->author}, aqui está o nosso canal de regras -> <#1130163573927198731>");
-        }
+        switch($content) {
+            case str_contains($content, '!regras'):
+                $message->channel->sendMessage("{$message->author}, ainda não temos um canal de regras!");
+                break;
+                
+            case str_contains($content, '!botão'):              
+                $button = Button::new(Button::STYLE_SUCCESS)
+                    ->setLabel('Aqui é um botão');
+                    
+                $button->setListener(function (Interaction $interaction) {
+                    $interaction->respondWithMessage(MessageBuilder::new()
+                        ->setContent('Apenas testando resposta do botão'));
+                }, $discord);
 
-        if(str_contains($content, '!cotação')) {
-            $url = "https://economia.awesomeapi.com.br/json/last/USD-BRL";
-            $cotação = json_decode(file_get_contents($url));
-            $real = "R$";
-            $message->reply("{$cotação->USDBRL->code}: {$real}{$cotação->USDBRL->bid}");
+                $row = ActionRow::new()
+                    ->addComponent($button);
+
+                $message->channel->sendMessage(MessageBuilder::new()
+                    ->setContent('Hello, world!')
+                    ->addComponent($row));                    
+                break;
+
+            case str_contains($content, '!cotação'):
+                $embed = new Embed($discord);
+                $embed->setTitle('Escolha abaixo qual moeda quer ver a cotação do dia')
+                      ->setColor( 0xFF0000 );                 
+                $message->channel->sendEmbed($embed);                
+
+                $url = "https://economia.awesomeapi.com.br/json/last/USD-BRL";
+                $cotação = json_decode(file_get_contents($url));
+                $real = "R$";                
+                $message->reply("{$cotação->USDBRL->code}: {$real}{$cotação->USDBRL->bid}");
+                break;
         }
     });
-
-    $discord->on(Event::VOICE_STATE_UPDATE, function (VoiceStateUpdate $state, Discord $discord, $oldstate) {
-        $channel = $discord->getChannel(1129957816229167208);
-        $channel->sendMessage("{$state->member->nick} entrou no chat de voz: {$state->channel->name}");
-    });
-
 });
     
 $discord->run();
